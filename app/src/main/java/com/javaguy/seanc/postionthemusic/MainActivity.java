fc895aa872abf3f74ba3f3aa7c103cff67d83cd1
seancohen <binyominco@gmail.com>
2017-05-18 22:37:54 -0400

    started making menu

--- app/src/main/java/com/javaguy/seanc/postionthemusic/MainActivity.java
+++ app/src/main/java/com/javaguy/seanc/postionthemusic/MainActivity.java
@@ -10,6 +10,8 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -94,6 +96,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return true;
     }
 
     @Override
